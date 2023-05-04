172c1c8cd468d267c2fb927363be3faac8283f64
NikitaNikandrov <nnikandroff@yandex.ru>
2023-05-04 10:14:39 +0300

    Initial Commit

---
+++ ColotScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColotScanner
+//
+//  Created by Никита on 04.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
