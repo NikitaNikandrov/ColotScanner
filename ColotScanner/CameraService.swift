aa87bf25716bba300b1b930c798ca0ed5b898ac0
NikitaNikandrov <nnikandroff@yandex.ru>
2023-05-04 13:03:44 +0300

    feature: added CameraService

---
+++ ColotScanner/CameraService.swift
@@ -0,0 +1,70 @@
+//
+//  CameraService.swift
+//  ColotScanner
+//
+//  Created by Никита on 04.05.2023.
+//
+
+import Foundation
+import AVFoundation
+
+class CameraService {
+    
+    var captureSession: AVCaptureSession?
+    var delegate: AVCapturePhotoCaptureDelegate?
+    
+    let output = AVCapturePhotoOutput()
+    let previewLayer = AVCaptureVideoPreviewLayer()
+    
+    func start(delegate: AVCapturePhotoCaptureDelegate, completion: @escaping (Error?) -> ()) {
+        self.delegate = delegate
+    }
+    
+    func captureFoto(with settings: AVCapturePhotoSettings = AVCapturePhotoSettings()) {
+        output.capturePhoto(with: settings, delegate: delegate!)
+    }
+    
+    private func setupCamera(completion: @escaping (Error?) -> ()) {
+        let session = AVCaptureSession()
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                if session.canAddInput(input) {
+                    session.addInput(input)
+                }
+                
+                if session.canAddOutput(output) {
+                    session.addOutput(output)
+                }
+                
+                previewLayer.videoGravity = .resizeAspectFill
+                previewLayer.session = session
+                session.startRunning()
+                self.captureSession = session
+                
+            } catch {
+                completion(error)
+            }
+        }
+    }
+    
+    private func checkPermissions(completion: @escaping (Error?) -> ()){
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                guard granted else { return }
+                DispatchQueue.main.async {
+                    self?.setupCamera(completion: completion)
+                }
+            }
+        case .restricted:
+            break
+        case .denied:
+            break
+        case .authorized:
+            setupCamera(completion: completion)
+        @unknown default:
+            break
+        }
+    }
+}
--- ColotScanner/MainViewController.swift
+++ ColotScanner/MainViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    let cameraService = CameraService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
